e22c499f4a61d16b413ce9835e75d9ef3050a2a0
siarikalo <rodargu76@yahoo.com>
2023-11-18 22:13:20 -0600

    my_first_rep

---
+++ program.py
@@ -0,0 +1,12 @@
+# Definir una lista de números
+lista = [1, 2, 3, 4, 5]
+
+# Inicializar una variable para almacenar la suma
+suma = 0
+
+# Iterar a través de la lista y sumar los elementos
+for elemento in lista:
+    suma += elemento
+
+# Mostrar la suma
+print("La suma de los elementos de la lista es:", suma)
